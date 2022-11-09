a2c5be17c49f5573e55ae50a0c22e3a25dd38167
Xianli1005 <117620803+Xianli1005@users.noreply.github.com>
2022-11-09 13:52:16 +0800

    Add files via upload

---
+++ 最优值.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int max(int a[],int n)
+{
+int sum2=0,maxsum=0;
+for(int i=0;i<n;i++)
+{
+	if(sum2<0)
+		sum2=a[i];
+	else
+		sum2+=a[i];
+if(maxsum<sum2)
+	maxsum=sum2;
+}
+ return maxsum;
+}
+
+int main()
+{
+	int i ,N,maxsum;
+	int num[100];
+	scanf("%d",&N);
+	for(i=0;i<N;i++)
+		scanf("%d",&num[i]);
+	maxsum=max(num,N);
+	return 0;
+}
